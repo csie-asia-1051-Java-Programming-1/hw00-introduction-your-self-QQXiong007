8e2377f5f6db51b4c77bfd6b1957b1ae5b5cf918
QQXiong007 <105021017@live.asia.edu.tw>
2017-02-24 15:07:17 +0800

    Homework01

---
+++ src/hw_01.java
@@ -0,0 +1,12 @@
+
+public class hw_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Topic:自我介紹
+	    //Time:2017/02/24
+		//作者:熊柏誠
+		System.out.println("我的名字:熊柏誠,系所:資訊工程學系,學號:105021017");
+	}
+
+}
